123d18a5a8479742692cb75c84e2bba14d9ce7f7
SuycxZMZ <suycx65@163.com>
2025-05-21 13:19:21 +0800

    删除一些费代码

--- quickEnv.sh
+++ quickEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Python packages using pip with Tsinghua mirror
-pip install timm==1.0.7 thop efficientnet_pytorch==0.7.1 einops grad-cam==1.5.4 dill==0.3.8 albumentations==1.4.11 pytorch_wavelets==1.3.0 tidecv PyWavelets opencv-python causal-conv1d>=1.2.0 mamba-ssm -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install timm==1.0.7 thop efficientnet_pytorch==0.7.1 einops grad-cam==1.5.4 dill==0.3.8 albumentations==1.4.11 pytorch_wavelets==1.3.0 tidecv PyWavelets opencv-python  -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # Install and upgrade openmim
 pip install -U openmim -i https://pypi.tuna.tsinghua.edu.cn/simple
