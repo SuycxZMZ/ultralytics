aad97ca23cd60232e3d51f8845ef0fcfd1537bba
SuycxZMZ <suycx65@163.com>
2025-05-27 14:04:55 +0800

    添加姿态检测示例

---
+++ predict-pose.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+ 
+# 加载预训练的YOLOv11n模型
+model = YOLO(r"yolo11x-pose.pt")
+ 
+# 对'bus.jpg'图像进行推理，并获取结果
+results = model.predict(r"/Users/yuansu/Desktop/codes/ultralytics/000000001/VID_20250520_174927.mp4", save=True, imgsz=640, conf=0.5, )
+ 
+# 处理返回的结果
+for result in results:
+    keypoints = result.keypoints       # 获取关键点估计信息
+    result.show()                      # 显示结果
+ 
