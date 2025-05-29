c9f42f15f7616dca6d3a157362deb5ad2a0f5cb8
SuycxZMZ <suycx65@163.com>
2025-05-29 10:31:26 +0800

    环境配置脚本更新

--- quickEnv.sh
+++ quickEnv.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Install Python packages using pip with Tsinghua mirror
-pip install timm==1.0.7 thop efficientnet_pytorch==0.7.1 einops grad-cam==1.5.4 dill==0.3.8 albumentations==1.4.11 pytorch_wavelets==1.3.0 tidecv PyWavelets opencv-python  -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install timm==1.0.7 thop efficientnet_pytorch==0.7.1 einops grad-cam==1.5.4 dill==0.3.8 albumentations==1.4.11 pytorch_wavelets==1.3.0 tidecv PyWavelets opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # Install and upgrade openmim
 pip install -U openmim -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip install ultralytics -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # Install mmengine and mmcv using mim
 mim install mmengine -i https://pypi.tuna.tsinghua.edu.cn/simple
